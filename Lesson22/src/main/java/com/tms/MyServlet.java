0ed119794836fe35c2ecfb09df063a940f823770
ODAX <notprogramm@mail.ru>
2023-02-18 13:45:17 +0300

    Lesson22

--- Lesson22/src/main/java/com/tms/MyServlet.java
+++ Lesson22/src/main/java/com/tms/MyServlet.java
@@ -1,7 +1,6 @@
 package com.tms;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +9,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletOutputStream outputStream = resp.getOutputStream();
-        outputStream.println("Hello from my first servlet");
-        outputStream.close();
+        req.getRequestDispatcher("/home.html").forward(req,resp);
+
     }
 }
